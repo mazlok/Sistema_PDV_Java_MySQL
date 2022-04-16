267d4e413ce4509196f35d3ce61691beac8789d5
brunagcoutinho <>
2022-04-16 15:37:21 -0300

    Commit 1.0
    Telas Projeto Int II

---
+++ src/main/java/Produto.java
@@ -0,0 +1,39 @@
+
+public class Produto {
+    
+    String nome, desc;
+    int codigo, quantidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+}
---
+++ src/main/java/Vendedor.java
@@ -0,0 +1,23 @@
+
+public class Vendedor {
+    
+    String nome;
+    int vendas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVendas() {
+        return vendas;
+    }
+
+    public void setVendas(int vendas) {
+        this.vendas = vendas;
+    }
+    
+}
